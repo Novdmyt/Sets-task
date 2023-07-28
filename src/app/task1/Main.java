9722bc0b26c520f9a00092e040798b818a4d9c59
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-28 22:27:50 +0300

    Add method getData

---
+++ src/app/task1/Main.java
@@ -0,0 +1,20 @@
+package app.task1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+
+    public static Set<String> getData() {
+        Set<String> data = new HashSet<>();
+        data.add("orange");
+        data.add("apple");
+        data.add("kiwi");
+        data.add("strawberry");
+        data.add("mango");
+        return data;
+    }
+
+
+}
